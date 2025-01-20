6d3560ccd75577ffbdb302f3e83f816bf4bf6b2e
Vivek Rawal <vkrawal2004@gmail.com>
2025-01-20 11:58:18 +0000

    Rotating matrix 90 degree

---
+++ 06_RotateMatrix.js
@@ -0,0 +1,24 @@
+let matrix=[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]  
+
+    // [
+    //     [7, 4, 1],
+    //     [8, 5, 2], 
+    //     [9, 6, 3]
+    //     ] 
+    
+    let newArr= [[],[],[]]
+    function rotateMatrix(rM){
+        
+        for(let i =0;i<=matrix.length-1 ;i++){
+            for(let j = matrix.length-1 ; j>=0 ; j--){
+                newArr[i].push(rM[j][i])
+            }
+        }
+        return newArr
+    }
+
+    console.log(rotateMatrix(matrix))
