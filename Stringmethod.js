c9cae491d9dd9724a9d538bed553a2a11cd64a79
Vivek Rawal <vkrawal2004@gmail.com>
2025-01-21 18:02:25 +0530

    Add files via upload

---
+++ Stringmethod.js
@@ -0,0 +1,17 @@
+let Str = 'Hey Vivek rawal Here '
+let End="!!" 
+ console.log(Str.charAt(7))
+ console.log(Str.charCodeAt(1));//askii value
+ console.log(Str.concat(End));
+ console.log(Str.endsWith( "!!" ));
+ console.log(Str.includes("a",1))
+ console.log(Str.lastIndexOf("e"))
+ console.log(Str.split(""))
+ console.log(Str.indexOf('a'));
+ console.log(Str.padEnd(1,'.'))
+ console.log(Str.padStart(25,'.'))
+ console.log(Str.repeat(2))
+ console.log(Str.slice(2,8))
+ console.log(Str.toUpperCase())
+ console.log(Str.trimEnd())
+ console.log(Str)
